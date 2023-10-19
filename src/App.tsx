e4d3a585d53e61eb20a948883bb9efe041ba62f6
SantiCombina <santicombina@gmail.com>
2023-10-19 15:11:37 -0300

    Added user selection page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import {Route, Routes} from "react-router-dom";
 import {Layout} from "./components/layout/layout";
 import {Detail} from "./views/detail";
 import {Home} from "./views/home";
+import {UserSelection} from "./views/user-selection";
 
 function App() {
     return (
         <Routes>
             <Route element={<Layout />}>
-                <Route element={<Home />} path="/" />;
+                <Route element={<UserSelection />} path="/" />;
+                <Route element={<Home />} path="/home" />;
                 <Route element={<Detail />} path="/detail/:id" />;
             </Route>
         </Routes>
