c804f1ac067a5db69ca7f22e1d04de63a6943cbd
SantiCombina <santicombina@gmail.com>
2023-10-23 23:27:37 -0300

    Changed movie list numbers

--- src/components/movie-list-number.tsx
+++ src/components/movie-list-number.tsx
@@ -24,22 +24,31 @@ export function MovieListNumber({sectionName}: Props) {
     }, [param.id]);
 
     return (
-        <div className="z-20 flex flex-col text-[#E5E5E5] text-3xl max-w-full overflow-hidden h-full">
+        <div className="z-20 flex flex-col text-[#E5E5E5] text-3xl max-w-full h-full">
             <h3 className="py-4 font-netflix-medium">{sectionName}</h3>
-            <div className="flex gap-2">
+            <div className="flex gap-2 h-[210px] w-full">
                 {movies?.map(
                     (movie, index) =>
                         index <= 9 && (
                             <div key={movie.id} className="flex items-center min-w-[293px] h-[210px] relative">
                                 <h2
-                                    className="text-[210px] text-black w-full block"
+                                    className={`text-[285px] flex items-center h-[210px] text-black font-netflix-bold pb-3 leading-none 
+                                    ${index === 0 ? "pl-8" : index <= 8 ? "pl-4" : ""} ${
+                                        index === 9 ? "text-[200px]" : ""
+                                    }`}
                                     style={{WebkitTextStroke: "5px #595959"}}
                                 >
-                                    {index + 1}
+                                    {index !== 9 ? (
+                                        index + 1
+                                    ) : (
+                                        <>
+                                            1 <span className="-ml-8">0</span>
+                                        </>
+                                    )}
                                 </h2>
                                 <img
                                     alt=""
-                                    className="w-auto max-w-[140px] rounded absolute right-0 top-0"
+                                    className="max-h-[210px] rounded absolute right-0 top-0"
                                     src={`http://image.tmdb.org/t/p/w500${movie?.poster_path}`}
                                 />
                             </div>
