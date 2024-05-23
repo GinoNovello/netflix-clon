9380e009b333dc324a5c4074f5097f281327a2ab
SantiCombina <santicombina@gmail.com>
2024-05-23 09:25:37 -0300

    Update user selection page layout and styles

--- src/views/user-selection.tsx
+++ src/views/user-selection.tsx
@@ -8,21 +8,23 @@ export function UserSelection() {
     const textTranslated = userSelectionTranslate[language];
 
     return (
-        <div className="flex flex-col items-center justify-center w-screen h-screen text-white bg-custom-black font-netflix">
-            <span className="text-[67px] py-11">{textTranslated.question}</span>
+        <div className="flex flex-col items-center justify-center w-full min-h-screen text-white gap-14 bg-custom-black font-netflix">
+            <span className="md:text-[3.5vw] sm:max-w-full max-w-[300px] text-[30px] text-center leading-9">
+                {textTranslated.question}
+            </span>
             <Link
-                className="text-[#808080] relative hover:text-custom-white text-2xl gap-3 flex flex-col items-center"
-                to={"/home"}
+                className="group text-[#808080] relative hover:text-custom-white text-base md:text-[1.5vw] gap-3 flex flex-col items-center"
+                to="/home"
             >
-                <div className="absolute top-0 left-0 w-[192px] h-[192px] bg-transparent hover:border-custom-white hover:border-[4px] rounded" />
-                <img alt="user image" className="w-[192px] rounded" src="/User1.png" />
-                <span>{textTranslated.user}</span>
+                <div className="relative flex flex-col items-center">
+                    <div className="absolute top-0 left-0 w-full h-full border-4 border-transparent rounded group-hover:border-custom-white" />
+                    <img alt="user image" className="max-w-[192px] min-w-[100px] w-[10vw] rounded" src="/User1.png" />
+                </div>
+                <span className="text-base md:text-[1.5vw]">{textTranslated.user}</span>
             </Link>
-            <div className="pt-24">
-                <button className="text-[#808080] text-2xl py-3 px-8 outline-1 outline hover:text-white">
-                    {textTranslated.manage}
-                </button>
-            </div>
+            <button className="text-[#808080] tracking-widest text-sm md:text-[1.2vw] py-3 px-8 outline-1 outline hover:text-white -mb-8 mt-8">
+                {textTranslated.manage}
+            </button>
         </div>
     );
 }
