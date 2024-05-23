48e115b6a8d6142411445d44bb3b92613d8573ea
SantiCombina <santicombina@gmail.com>
2024-05-23 07:51:10 -0300

    Optimize useScroll hook and add useCallback

--- src/hooks/useScroll.tsx
+++ src/hooks/useScroll.tsx
@@ -1,16 +1,19 @@
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 
 export const useScroll = ({scrollSize}: {scrollSize: number}) => {
     const [isVisible, setIsVisible] = useState(false);
 
-    const toggleVisibility = () => {
-        window.scrollY >= scrollSize ? setIsVisible(true) : setIsVisible(false);
-    };
+    const toggleVisibility = useCallback(() => {
+        setIsVisible(window.scrollY >= scrollSize);
+    }, [scrollSize]);
 
     useEffect(() => {
         window.addEventListener("scroll", toggleVisibility);
-        () => window.removeEventListener("scroll", toggleVisibility);
-    }, []);
+
+        return () => {
+            window.removeEventListener("scroll", toggleVisibility);
+        };
+    }, [toggleVisibility]);
 
     return {isVisible};
 };
